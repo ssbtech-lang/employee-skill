ec38a02fee70284d5f1dc974db5436370e2b1d90
ruthvireddyk <ruthvireddy05@gmail.com>
2026-07-19 22:45:45 +0530

    "resolved"

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI is:", process.env.MONGO_URI); // 👈 Debug line
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
